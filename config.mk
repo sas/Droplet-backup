fe40dab87c4fdd92ea4f88111424242e8748d1d9
Stephane Sezer <sas@cd80.net>
2011-02-19 12:53:31 +0100

    The projet is now linked with libdroplet.

--- config.mk
+++ config.mk
@@ -1,5 +1,19 @@
 # customize below to fit your system
+DPL_PKG_CONFIG	= /usr/local/lib/pkgconfig/
 
-CC	= gcc
-CFLAGS	= -Wall -Wextra -Werror -g
-LDFLAGS	=
+# compiler and flags
+CC		= gcc
+CFLAGS		= -Wall -Wextra -Werror -g
+LDFLAGS		=
+MAKEFLAGS	= --silent
+
+# required flags
+CFLAGS		= -I. -std=c99
+
+# openssl
+CFLAGS		+= `pkg-config --cflags openssl`
+LDFLAGS		+= `pkg-config --libs openssl`
+
+# libdroplets
+CFLAGS		+= `PKG_CONFIG_PATH=$(DPL_PKG_CONFIG) pkg-config --cflags droplet-1.0`
+LDFLAGS		+= `PKG_CONFIG_PATH=$(DPL_PKG_CONFIG) pkg-config --libs droplet-1.0`
