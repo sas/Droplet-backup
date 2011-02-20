7db40f97c14425b694950fce42c6dc9cc0775f01
Stephane Sezer <sas@cd80.net>
2011-02-20 09:48:09 +0100

    usage() improvement and new warn() and err() functions.
    usage() now contains a few examples for dplbck to help the user.
    
    warn() and err() functions should be used for all warning and logging.
    This should allow future modification of dplbck to include, for example,
    a "log to file" option.

--- src/usage.c
+++ src/usage.c
@@ -1,12 +1,15 @@
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "usage.h"
 
 void usage(FILE *output)
 {
   char *message[] = {
-    "usage: dplbck [ options ] <command> [ command_options ] [ command_args ]",
+    "usage: dplbck [ options ] <command> [ command_args ]",
     "    options",
+    "        --profile-dir, -d: specify the droplet profile drectory to use",
     "        --profile, -p:     specify the droplet profile to use",
     "        --verbose, -v:     verbose mode",
     "        --interactive, -i: interactive mode",
@@ -16,8 +19,36 @@ void usage(FILE *output)
     "        list:    list all available backups",
     "        stats:   display stats about a backup folder",
     "        help:    display this help",
+    "i.e.:",
+    "    dplbck -v backup /etc dpl://my_backups/backup_folder/",
+    "        makes a backup of /etc in the bucket `my_backups', in the folder",
+    "        `backup_folder' and enables verbose mode, so all transferts are",
+    "        logged on stderr.",
+    "    dplbck -p bck restore dpl://my_save/websrv/htdocs/ /srv/www/htdocs/",
+    "        uses the profile `bck' to restore the latest backup present in",
+    "        the folder `/websrv/htdocs' in the bucket `my_save' in the",
+    "        folder `/srv/www/htdocs/'.",
   };
 
   for (unsigned int i = 0; i < sizeof (message) / sizeof (message[0]); ++i)
     fprintf(output, "%s\n", message[i]);
+}
+
+void err(int exit_val, const char *format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+  vfprintf(stderr, format, ap);
+  va_end(ap);
+  exit(exit_val);
+}
+
+void warn(const char *format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+  vfprintf(stderr, format, ap);
+  va_end(ap);
 }
--- src/usage.h
+++ src/usage.h
@@ -4,5 +4,7 @@
 # include <stdio.h>
 
 void usage(FILE *output);
+void err(int exit_val, const char *format, ...);
+void warn(const char *format, ...);
 
 #endif /* !USAGE_H_ */
