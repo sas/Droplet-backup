9cda08c1b1a8fc463801b1474864916d4d916831
Stephane Sezer <sas@cd80.net>
2011-03-24 02:40:00 +0100

    Removed the hash_file() functions.
    These are not going to be needed in the future, due to changes in the
    storage method.

--- src/objects/hash_file.h
+++
@@ -1,8 +0,0 @@
-#ifndef HASH_FILE_H_
-# define HASH_FILE_H_
-
-# include <openssl/sha.h>
-
-int hash_file(const char *filename, unsigned char hash[SHA256_DIGEST_LENGTH]);
-
-#endif /* !HASH_FILE_H_ */
